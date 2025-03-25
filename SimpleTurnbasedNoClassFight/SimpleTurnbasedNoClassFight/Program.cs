075eb64400ddf7a3d230bd5c2f62c6a710a67e28
Graham Arnett <GrahamKentArnett@outlook.com>
2025-03-25 14:55:25 -0400

    git init

---
+++ SimpleTurnbasedNoClassFight/SimpleTurnbasedNoClassFight/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SimpleTurnbasedNoClassFight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Turn based fight sim");
+
+            string choice;
+
+            int playHealth = 100;
+            int enemHealth = 100;
+            int slashDam = 11;
+            int zapDam = 16;
+            int healAmt = 20;
+
+            do
+            {
+                Console.Write("Would you like to go again? (y/n): ");
+            choice = Console.ReadLine().ToLower();
+            }
+            while (true);
+        }
+    }
+}
