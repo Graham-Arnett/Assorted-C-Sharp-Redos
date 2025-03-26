f85562626e1365f42f838c320a9bf4acf3a12302
Graham Arnett <GrahamKentArnett@outlook.com>
2025-03-26 15:51:41 -0400

    still working, but done for today

--- SimpleTurnbasedNoClassFight/SimpleTurnbasedNoClassFight/Program.cs
+++ SimpleTurnbasedNoClassFight/SimpleTurnbasedNoClassFight/Program.cs
@@ -8,18 +8,40 @@
 
             string choice;
             Random random = new Random();
-            int playHealth = 100;
-            int enemHealth = 100;
-            int slashDam = 11;
-            int zapDam = 16;
-            int cureAmt = 20;
-            char[] actions = {'S','C','Z'};
+           
+            char[] actions = {'s','c','z'};
+            char act;
             do
             {
-                Console.WriteLine("You wander into an abandoned castle, your hand hovering above the hilt of your shortsword at your side. As you ascend the cobalt carpeted stairs, the Bandit King leaps from his throne. Prepare to fight!");
+                int enemRoll = random.Next(1,4);
+                int playHealth = 100;
+                int enemHealth = 100;
+                int slashDam = 11;
+                int zapDam = 16;
+                int cureAmt = 20;
+                Console.WriteLine("You wander into an abandoned castle, your hand hovering above the hilt of your shortsword at your side. As you ascend the cobalt carpeted stairs, the Bandit King leaps from his throne. Prepare to fight!" +
+                    "\nPress s to slash, c to cure, or z to zap");
                 while(playHealth > 0 | enemHealth > 0)
                 {
+                    Console.Write($"Your turn. Your health is currently {playHealth}/100. What will you do? ");
+                    act = Console.ReadKey().KeyChar;
+                    switch (act)
+                    {
+                        case 's':
+                            Console.WriteLine($"You take your shortsword and expertly slash at the brigand. You deal {slashDam} damage!");
+                            enemHealth = enemHealth - slashDam;
+                            break;
 
+                        case 'c':
+                            Console.WriteLine($"You take a small blue potion from your satchel and down the whole thing. You recover {cureAmt} health and are now at {playHealth}/100!");
+                            playHealth = playHealth + cureAmt;
+                            break;
+
+                        case 'z':
+                            Console.WriteLine($"You press your thumb and index fingers together and make a deafening snap. You deal {zapDam} damage!");
+                            enemHealth = enemHealth - zapDam;
+                            break;
+                    }
                 }
                 Console.Write("Would you like to go again? (y/n): ");
             choice = Console.ReadLine().ToLower();
