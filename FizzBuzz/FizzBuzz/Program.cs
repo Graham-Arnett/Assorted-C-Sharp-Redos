b85575fd5f4efc5c76a8afcf1f440cee8942ff5c
Graham Arnett <GrahamKentArnett@outlook.com>
2025-04-02 21:18:17 -0400

    its ok, I was really tired and stressed today so I couldn't do a lot

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection.Metadata;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to fizzbuzz!");
+
+            char choice;
+
+            do
+            {
+
+                Console.Write("\nEnter a number please: ");
+                int num = int.Parse(Console.ReadLine());
+                if (num % 5 == 0 && num % 3 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("FIZZBUZZ");
+                }
+                else if (num % 5 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("FIZZ");
+                }
+                else if(num % 3 == 0)
+                {
+                    Console.ForegroundColor= ConsoleColor.Blue;
+                    Console.WriteLine("BUZZ");
+                }
+                else
+                {
+                    Console.WriteLine("That isn't fizz or buzz");
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("\nWould you like to play again? (y/n): ");
+                choice = Console.ReadKey().KeyChar;
+            } while (choice == 'y' || choice == 'Y');//idk how to do the ignores case stuff to a char
+        }
+    }
+}
