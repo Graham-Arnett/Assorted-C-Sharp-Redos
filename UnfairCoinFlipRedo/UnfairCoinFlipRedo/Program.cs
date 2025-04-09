5cc3fa454b5b43ac741baf25a4846847a454763b
Graham Arnett <GrahamKentArnett@outlook.com>
2025-04-08 21:39:32 -0400

    still working on this, I think my while loop's logic is just unusable

---
+++ UnfairCoinFlipRedo/UnfairCoinFlipRedo/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace UnfairCoinFlipRedo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unfair coinflip app" +
+               "\nYou were walking home from work and decided to take the scenic route. " +
+               "\nWhen you were at the foot of the last bridge to cross, a hideous troll approached and demanded you played a game with it." +
+               "\nA coinflip, heads means you pass, but tails means you have to listen to the insufferable creature.");
+
+            char redo;
+            string[] responses = {"Huh? How did you win that fast?", "You're terrible at this!", "You smell of fish and insecurity!", "Your friends keep you around to look better by proxy!"
+            ,"Your parents change the conversation when your name comes up!"};
+            Random random = new Random();
+            bool heads = false;
+            do
+            {
+                int timesFlip = 0;
+                int coinChance = random.Next(1, 5);
+                //while (heads == false)
+                //{
+                //    Console.WriteLine("Lets flip the coin!");
+                //    if (coinChance == 1)
+                //    {
+                //        Console.WriteLine(responses[0]);
+                //        heads = true;
+                //    }
+                //    else
+                //    {
+                //        Console.WriteLine(timesFlip);
+                //    }
+                //    timesFlip++;
+                //}
+
+                Console.Write("Would you like to go again? (y/n): ");
+                redo = Console.ReadKey().KeyChar;
+            } while (redo == 'y' || redo == 'Y');
+        }
+    }
+}
