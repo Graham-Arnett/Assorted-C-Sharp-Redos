381b51dac8d9ce762ec8ca14e20d38d976f735b3
Graham Arnett <GrahamKentArnett@outlook.com>
2025-04-18 17:40:33 -0400

    there were some hiccups today, I tried to get a file io version working but it seems I don't quite understand that yet. I will come back and finish this tomorrow

--- WordleRedo/WordleRedo/Program.cs
+++ WordleRedo/WordleRedo/Program.cs
@@ -1,4 +1,15 @@
-﻿namespace WordleRedo
+﻿using static System.Reflection.Metadata.BlobBuilder;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Net.NetworkInformation;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace WordleRedo
 {
     internal class Program
     {
@@ -9,6 +20,11 @@
             char redo;
             Random random = new Random();
             bool gameOver = false;
+            string[] words = {"ANTSY","AMPLE","AGILE","AFTER","BLOCK","BLACK","BLOAT","BLANK","BOOTS","BOATS",
+"BRINE","BRICK","BONES","BONEY","CLACK","CLOAK","COATS","COMBS","CHILL","CRIMP","CHIMP","CHAMP","CHOMP",
+"DEALT","DYING","DEEMS","EXTRA","EXERT","FIGGY","FOOLS","GREAT","GHOUL","GHAST","GOATS","GRATE","GUMBO",
+"HOOPS","HYMNS","HOLES","HOODS","INNER","INTER","INERT","JOKES","JAPES","JUKED","LUMPS","LOOPS",
+"LEAPS","MOATS","MILES","MOONS","MOUNT","MEANS","MODES","MOOBS","MOODS","NEWSY","NOTCH","NICHE",};
             bool victory = false;//probably will not keep this variable, but I'm using this to help me code the skeleton here
             //this time we are getting the words from file io, and if you win a round you get to add a new FIVE letter word
             do
@@ -16,6 +32,11 @@
                 //we need a for loop to iterate through the chosen word and match the letters at position
                 //we need to change the colors depending on if that letter is in the right spot or not
                 //it would be cool if we could straight up limit entering more or less than 5 letters
+
+                int pickedWord = random.Next(words.Length);
+                string answer = words[pickedWord];
+               
+
                 Console.ForegroundColor = ConsoleColor.White;
                 if (victory == true)
                 {
