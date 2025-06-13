ca14de84259efd31eee3d90ef0bd0f16f8e16bbf
Graham Arnett <GrahamKentArnett@outlook.com>
2025-06-13 14:50:03 -0400

    The evennumCheck app is done

---
+++ EvenNumCheck/EvenNumCheck/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace EvenNumCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my even number checker!");
+            char redo;
+
+            do
+            {
+                Console.Write("\nEnter a number please: ");
+                double entry = double.Parse(Console.ReadLine());
+                if (entry % 2 == 0)
+                {
+                    Console.WriteLine("Yeah, that's even.");
+                }
+                else
+                {
+                    Console.WriteLine("Nah, its odd.");
+                }
+                Console.Write("\n\nWould you like to go again? (y/n): ");
+                redo = Console.ReadKey().KeyChar;
+            } while (redo == 'y' || redo == 'Y');
+        }
+    }
+}
