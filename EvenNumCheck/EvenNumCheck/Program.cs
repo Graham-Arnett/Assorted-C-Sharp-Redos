0290f1fac6b46004071ea1d0f1036119230a2223
Graham Arnett <GrahamKentArnett@outlook.com>
2025-06-14 15:24:59 -0400

    the even number checker now has validation

--- EvenNumCheck/EvenNumCheck/Program.cs
+++ EvenNumCheck/EvenNumCheck/Program.cs
@@ -6,11 +6,17 @@
         {
             Console.WriteLine("Welcome to my even number checker!");
             char redo;
-
+            char[] acceptChars = {'y','Y','n','N'};
+            double entry = 0;
             do
             {
                 Console.Write("\nEnter a number please: ");
-                double entry = double.Parse(Console.ReadLine());
+                //double entry = double.Parse(Console.ReadLine());
+                while(!double.TryParse(Console.ReadLine(), out entry))
+                {
+                    Console.WriteLine("Invalid format, please enter a number!");
+                    Console.Write("\nEnter a number please: ");
+                }
                 if (entry % 2 == 0)
                 {
                     Console.WriteLine("Yeah, that's even.");
@@ -19,9 +25,21 @@
                 {
                     Console.WriteLine("Nah, its odd.");
                 }
+                bool validChar = false;
+                do { 
                 Console.Write("\n\nWould you like to go again? (y/n): ");
                 redo = Console.ReadKey().KeyChar;
+                    if (acceptChars.Contains(redo))
+                    {
+                       validChar = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nPlease try and enter a valid input again.\n");
+                    }
+                }while(!validChar);
             } while (redo == 'y' || redo == 'Y');
+            Console.WriteLine("Goodbye, thanks for playing!");
         }
     }
 }
