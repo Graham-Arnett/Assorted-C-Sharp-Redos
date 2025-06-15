7d337541d6b8a953d6b5e12623db883549f9f090
Graham Arnett <GrahamKentArnett@outlook.com>
2025-06-15 18:17:58 -0400

    init commit riddle game

---
+++ riddlegameApp/riddlegameApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace riddlegameApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
