8478c32c3b493339bd3863f8d46215f01b0ca10e
Graham Arnett <GrahamKentArnett@outlook.com>
2025-06-20 16:09:52 -0400

    init commit woud count app

---
+++ WordCount/WordCount/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace WordCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This app is meant to count the number of words in a sentence");
+            char redo;
+            do
+            {
+                int wordCount = 0;
+                bool validSent = false;
+                while (!validSent)
+                {
+                    Console.WriteLine("Please enter a sentence, note that a sentence is considered as complete if there is a punctuation mark at the end.");
+                    string sentence = Console.ReadLine();
+                    
+                    if (sentence.EndsWith("!") || sentence.EndsWith("?") || sentence.EndsWith("."))
+                    {
+                        Console.WriteLine("That is a valid sentence, you can continue.");
+                        validSent = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("That is invalid. Please try again.");
+                    }
+                }
+                Console.Write("Would you like to go again? (y/n): ");
+                redo = Console.ReadKey().KeyChar;
+            } while (redo == 'y' || redo == 'Y');
+        }
+    }
+}
