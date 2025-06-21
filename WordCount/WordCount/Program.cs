b8558cac199849ea2c287467393badaba4f9a8a4
Graham Arnett <GrahamKentArnett@outlook.com>
2025-06-21 00:40:39 -0400

    wordcount app is complete and fully functional per my intentions

--- WordCount/WordCount/Program.cs
+++ WordCount/WordCount/Program.cs
@@ -12,9 +12,9 @@
                 bool validSent = false;
                 while (!validSent)
                 {
-                    Console.WriteLine("Please enter a sentence, note that a sentence is considered as complete if there is a punctuation mark at the end.");
+                    Console.WriteLine("\nPlease enter a sentence, note that a sentence is considered as complete if there is a punctuation mark at the end.");
                     string sentence = Console.ReadLine();
-                    
+
                     if (sentence.EndsWith("!") || sentence.EndsWith("?") || sentence.EndsWith("."))
                     {
                         Console.WriteLine("That is a valid sentence, you can continue.");
@@ -22,12 +22,21 @@
                     }
                     else
                     {
-                        Console.WriteLine("That is invalid. Please try again.");
+                        Console.WriteLine("\nThat is invalid. Please try again.");
+                    }
+                    for (int i = 0; i < sentence.Length; i++)
+                    {
+                        if (sentence[i] == ' ' || sentence[i] == '.' || sentence[i] == '?' || sentence[i] == '!')
+                        {
+                            wordCount++; //the thought process is if you spot a space, you just got to the end of the word, and you always put the punctuation right after a word
+                        }
                     }
                 }
+                Console.WriteLine($"Your sentence had {wordCount} words in it!");
                 Console.Write("Would you like to go again? (y/n): ");
                 redo = Console.ReadKey().KeyChar;
             } while (redo == 'y' || redo == 'Y');
+            Console.WriteLine("Goodbye, thanks for playing!");
         }
     }
 }
