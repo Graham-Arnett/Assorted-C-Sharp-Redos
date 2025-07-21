8c7f74483df051a8ea2bf401ce8be96ff57b7ace
Graham Arnett <GrahamKentArnett@outlook.com>
2025-07-20 23:06:03 -0400

    didn't have much time today, remade the simple name generator app to remind myself of how to use randoms

---
+++ NameGenApp/NameGenApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace NameGenApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name Generator App");
+            char redo;
+            Random random = new Random();
+            string[] first = {"Mary","John","Jean","Jose","Bob","Hugh","Kevin","Kyle","Linus","Peter","Simon","Myriam","Eleanor","Evan" +
+                    "Brian","Billy","Jenny"};
+            string[] last = {"Johnson","Greene","Brown","Swanson","Smith","Whittle","Skinner","Chalmers"};
+            do
+            {
+                int fChoice = random.Next(first.Length);
+                int lChoice = random.Next(last.Length);
+                Console.Write($"\n\nYour name is: {first[fChoice]} {last[lChoice]}" +
+                    $"\n\nWould you like to go again? (y/n): ");
+                redo = Console.ReadKey().KeyChar;
+            } while (redo == 'y' || redo == 'Y');
+        }
+    }
+}
