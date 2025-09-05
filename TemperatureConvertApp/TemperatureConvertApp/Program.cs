d37e7d414819225370019a38dc1abe8c6e31176e
Graham Arnett <GrahamKentArnett@outlook.com>
2025-09-04 21:28:42 -0400

    was bored and busy so I added kelvin now its really done

--- TemperatureConvertApp/TemperatureConvertApp/Program.cs
+++ TemperatureConvertApp/TemperatureConvertApp/Program.cs
@@ -11,7 +11,8 @@
                 Console.Write("Enter a temperature in fahrenheit: ");
                 double temp = double.Parse(Console.ReadLine());
                 double celsius = Math.Round(((temp - 32) * 5/9),2);
-                Console.WriteLine($"\nThat is {celsius} degrees!");
+                double kelvin = Math.Round((((temp - 32) * 5 / 9)+ 273.15), 2);
+                Console.WriteLine($"\nThat is {celsius} degrees in celsius and {kelvin} degrees in kelvin!");
                 Console.Write("\n\nWould you like to go again? (y/n): ");
                 redo = Console.ReadKey().KeyChar;
             } while (redo == 'y' || redo == 'Y');
